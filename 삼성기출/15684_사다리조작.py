7d32b8293233753302e4b79f6b4f3ae78c0d5eb9
jonghyeok <alb7979s@naver.com>
2020-04-14 15:44:24 +0900

    200414

--- 삼성기출/15684_사다리조작.py
+++ 삼성기출/15684_사다리조작.py
@@ -1,5 +1,7 @@
+#15684
 from sys import*
 input = stdin.readline
+
 def check():
     for i in range(m):
         s = i
@@ -8,27 +10,35 @@ def check():
             elif s-1 >=0 and a[j][s-1]: s-=1
         if s != i: return 0
     return 1
-def solve(cnt, x, y):
-    res = INF
-    if cnt>3: return res
-    if check(): return cnt
-    if cnt == 3: return res     #여기부분 
-    for i in range(x, n):       #여기부분 시간초과 원인
-        temp = y if x==i else 0
-        for j in range(temp, m-1):
-            if a[i][j]:
-                j+=1
-            else:
-                a[i][j]=1
-                res = min(res, solve(cnt+1, i, j))
-                a[i][j]=0
-    return res
+
+def solve(idx, cnt, t):
+    ans=INF
+    if idx == cnt:
+        if check(): return cnt
+        return ans
+    for i in range(t+1, len(can_list)):
+        x, y = can_list[i]
+        if a[x][y]==0 and a[x][y+1]==0:
+            a[x][y]=1
+            ans=min(ans, solve(idx+1, cnt, i))
+            a[x][y]=0
+    return ans
 
 INF = 1e9
 m,k,n = map(int,input().split())
 a=[[0]*m for _ in range(n)]
+can_list=[]
 for i in range(k):
     x, y = map(int,input().split())
     a[x-1][y-1] = 1
-ans=solve(0,0,0)
-print(ans if ans != INF else -1)
+for i in range(n):
+    for j in range(m-1):
+        if a[i][j]==0 and a[i][j+1]==0:
+            can_list.append((i,j))
+ans=INF
+for i in range(4):
+    ans = solve(0, i, -1)
+    if ans != INF:
+        print(ans)
+        break
+if ans==INF: print(-1)

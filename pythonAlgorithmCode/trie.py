662c845432fcb4be88fbfade33c9e3361fdfe74f
jonghyeok <alb7979s@naver.com>
2020-07-17 10:56:39 +0900

    200717

--- pythonAlgorithmCode/trie.py
+++ pythonAlgorithmCode/trie.py
@@ -1,52 +1,34 @@
+#https://www.acmicpc.net/problem/5052
+from sys import*
+setrecursionlimit(10**6)
+input = lambda:stdin.readline().strip()
 class Node:
-    def __init__(self, key, data=None):
-        self.key = key
-        self.data = data
-        self.children = {}
+    def __init__(self):
+        self.end = False
+        self.child = {}
+
 class Trie:
     def __init__(self):
-        self.root = Node(None)
+        self.root=Node()
     def insert(self, string):
-        cur_node = self.root
-        for char in string:
-            if char not in cur_node.children:   #?? 시간 ㄱㅊ?
-                cur_node.children[char] = Node(char)
-            cur_node = cur_node.children[char]
-        cur_node.data = string  #마지막 스트링 저장 공간 ㄱㅊ?
-    def search(self, string):
-        cur_node = self.root
-        for char in string:
-            if char in cur_node.children:
-                cur_node = cur_node.children[char]
-            else:
-                return False
-        if cur_node.data != None: return True
-    def starts_with(self, prefix):
-        cur_node = self.root
-        res = []
-        subtrie = None
-        for char in prefix:
-            if char in cur_node.children:
-                cur_node = cur_node.children[char]
-                subtrie = cur_node
-            else:
-                return None
-        v = list(subtrie.children.values())
-        while v:
-            cur = v.pop()
-            if cur.data != None:
-                res.append(cur.data)
-            v += list(cur.children.values())
-        return res
-t = Trie()
-words = ["romane", "romanus", "romulus", "ruben", 'rubens', 'ruber', 'rubicon', 'ruler']
-for word in words:
-    t.insert(word)
+        node = self.root
+        for i, char in enumerate(string):
+            if i == len(string)-1:
+                if char in node.child: return False
+            if char not in node.child: node.child[char] = Node()
+            node = node.child[char]
+            if node.end: return False
+        node.end = True
+        return True
+for tc in range(int(input())):
+    trie = Trie()
+    n = int(input())
+    YES=True
+    for i in range(n):
+        a=input()
+        if not YES:continue
+        if not trie.insert(a):
+            YES=False
+    if YES: print("YES")
+    else:print("NO")
 
-print(t.search("romulus"))
-print(t.search("ruler"))
-print(t.search("rulere"))
-print(t.search("romunus"))
-print(t.starts_with("ro"))
-print(t.starts_with("rube"))
-#출처 https://gist.github.com/osori/d0200b9bb7665d6a69da61b431e4077f

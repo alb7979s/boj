962fd718aeddc2eaed15d3efacf37f4290d41428
jonghyeok <alb7979s@naver.com>
2020-07-29 10:07:14 +0900

    200729

--- pythonAlgorithmCode/SCC.py
+++ pythonAlgorithmCode/SCC.py
@@ -1,15 +1,19 @@
 #2150
 #SCC
+from sys import*
+input=stdin.readline
+setrecursionlimit(10**6)
 def dfs(u):
+    global cnt, SN
     dfsn[u] = cnt
     cnt += 1
-    s.push(u)
+    s.append(u)
 
     res = dfsn[u]
     for v in adj[u]:
         #방문 x
         if(dfsn[v]==0): res = min(res, dfs(v))
-        #방문 했으니 SCC 추출 x
+        #방문 했으나 SCC 추출 x
         if(not finished[v]): res = min(res, dfsn[v])
 
     if res == dfsn[u]:
@@ -26,17 +30,18 @@ def dfs(u):
     return res
 V, E = map(int,input().split())
 adj = [[]*V for _ in range(V)]
+SCC, s = [], []
+dfsn, finished, sn = [0]*V, [0]*V, [0]*V
+SN, cnt = 1, 1
 for i in range(E):
     a,b = map(int,input().split())
     adj[a-1].append(b-1)
-SCC = []
-dfsn = [0]*V
-SN, cnt = 1, 1
 #DFS돌며 SCC 추출
 for i in range(V):
     if(not dfsn[i]): dfs(i)
 SCC.sort()
+print(len(SCC))
 for scc in SCC:
     for x in scc:
         print(x+1, end=' ')
-    print()
+    print(-1)
